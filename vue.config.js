89e3cb835758fe00350ed960c83f4f34bdb35b0f
anaelleltd <44770354+anaelleltd@users.noreply.github.com>
2021-06-14 17:13:09 +1000

    deploy test2

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,14 @@
-// vue.config.js file to be placed in the root of your repository GITHUB PAGES method
+/* vue.config.js file to be placed in the root of your repository GITHUB PAGES method
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
     ? '/kusamawatch/'
     : '/'
-}
+}*/
 
 // vue.config.js file to be placed in the root of your repository GITLAB PAGES method
 
-/*module.exports = {
+module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
     ? '/' + process.env.kusamawatch + '/'
-    : '/'
-}*/
+    : '/public/'
+}
